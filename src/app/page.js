7a942fc1ba628dd3d79c692d9156a3df8a693977
tapos-chandro <taposchandro231@gmail.com>
2025-07-30 23:32:51 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is a simple example of a Next.js application.</p>
+    </div>
+  );
+}
