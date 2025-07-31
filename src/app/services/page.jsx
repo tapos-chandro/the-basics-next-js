199c84cae5656f77f86c16307420804257e59fe4
tapos-chandro <taposchandro231@gmail.com>
2025-07-31 23:14:25 +0600

    Image Optimization has been done

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-export default function Services() {
+export default async function Services() {
+  const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
+  const meals = await res.json();
+
   return (
-    <div>This is Services</div>
-  )
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 p-4">
+      {meals?.meals?.map(meal => (
+        <div key={meal?.idMeal} className="rounded-lg overflow-hidden shadow-md">
+          <div className="relative w-full  h-60">
+            <Image
+              src={meal?.strMealThumb}
+              alt={meal?.strMeal || 'Meal Image'}
+              fill
+     	sizes="(max-width: 768px) 100vw, 33vw"
+              className="object-cover"
+            />
+            {/* <img src={meal?.strMealThumb} alt="" /> */}
+          </div>
+          <div className="p-2">
+            <h1 className="text-lg font-semibold">{meal?.strCategory}</h1>
+            <p className="text-sm text-gray-600">{meal?.strMeal}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
