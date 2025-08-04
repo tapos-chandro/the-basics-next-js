5fd77f189421844ecc52c0e599a5c70528ee7f70
tapos-chandro <taposchandro231@gmail.com>
2025-08-04 21:36:40 +0600

    Organize Files has been done

---
+++ src/app/(marketing)/marketing/layout.js
@@ -0,0 +1,12 @@
+export default function MarketingLayout({ children }) {
+
+  return <section>
+
+
+    {children}
+    <footer className="text-center p-4">
+      <p>© 2023 My Next.js App</p>
+    </footer>
+
+  </section>
+}
